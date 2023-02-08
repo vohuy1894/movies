e5b88186516fd746dcd4b4b12b373f2f46ae27ec
Huy Vo <vohuy1894@gmail.com>
2023-02-08 12:47:26 -0600

    Initial Commit

---
+++ src/main/java/dev/huy/movies/MovieService.java
@@ -0,0 +1,20 @@
+package dev.huy.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> findAllMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
